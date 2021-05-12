1623f4a96ef2e720988e22c4a981b1ba731dc5a0
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-05-12 21:08:48 +1000

    Add build_table method

--- lib/application.rb
+++ lib/application.rb
@@ -12,12 +12,27 @@ class Application
     confirm_user?
     response = @api.get_request('tickets?page[size]=25')
     populate_tickets(JSON.parse(response.body, :symbolize_names => true))
+    build_table
+
+  end
+
   def populate_tickets(json)
     json[:tickets].each do |ticket|
       @tickets << ticket
     end
   end
+
+  def build_table
+    table = TTY::Table.new(header: ["ID", "Subject", "Priority", "Status", "Last Updated"])
+    @tickets.each do |ticket|
+      ticket[:priority] ||= 'n/a'
+      # Remove time from datestamp and reverse order to DD/MM/YYYY
+      ticket[:updated_at] = ticket[:updated_at].split('T').first.split('-').reverse.join('-')
+      table << [ticket[:id], ticket[:subject], ticket[:priority], ticket[:status], ticket[:updated_at]]
+    end
+    puts table.render(:ascii, padding: [0,1,0,1])
   end
+
   def confirm_user?
     update = true
     while update
